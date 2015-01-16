3a9585412e2534bf3b8b7f99806e9be411a2464f
Cornelius Lilge <cornelius@wasis.nu>
2015-01-16 22:36:15 +0100

    Moved archetype search from dao to service

--- app/models/daos/slick/ArchetypeDaoSlick.scala
+++ app/models/daos/slick/ArchetypeDaoSlick.scala
@@ -7,7 +7,6 @@ import play.api.db.slick.Config.driver.simple._
 import models.daos.slick.ArchetypeSlickDB._
 import play.api.Play.current
 import play.api.Logger
-import org.apache.maven.artifact.versioning.ComparableVersion
 import models.Archetype
 
 class ArchetypeDaoSlick extends ArchetypeDao {
@@ -30,41 +29,5 @@ class ArchetypeDaoSlick extends ArchetypeDao {
       )}
     }
   }
-  
-  def find(groupId: Option[String], artifactId: Option[String], version: Option[String]): List[Archetype] = {
-    // This will be slow as hell, but I cannot slick, so...
-    val archetypes = findAll.filter { a =>
-      if (groupId.isDefined) {
-        a.groupId.toLowerCase().contains(groupId.get.toLowerCase())
-      } else {
-        true
-      }
-    }.filter { a =>
-      if (artifactId.isDefined) {
-        a.artifactId.toLowerCase().contains(artifactId.get.toLowerCase())
-      } else {
-        true
-      }
-    }.filter { a =>
-      if (version.isDefined && version.get != "newest") {
-        a.version.toLowerCase().contains(version.get.toLowerCase())
-      } else {
-        true
-      }
-    }
-    if (version.isDefined && version.get == "newest") {
-      archetypes.groupBy( a => (a.groupId, a.artifactId)).flatMap {
-        case ((groupId, artifactId), list) => {
-          list.sortWith((a1, a2) => {
-            0 < a1.compareTo(a2)
-          }).take(1)
-        }
-      }.toList
-    } else {
-      archetypes
-    }
-  }
-  
-  implicit def archetypeToComparableVersion(a: Archetype) : ComparableVersion = new ComparableVersion(a.version)
   
 }
--- app/services/impl/ArchetypesServiceImpl.scala
+++ app/services/impl/ArchetypesServiceImpl.scala
@@ -13,6 +13,7 @@ import scala.concurrent.duration.Duration
 import scala.concurrent.Await
 import javax.inject.Inject
 import models.daos.ArchetypeDao
+import org.apache.maven.artifact.versioning.ComparableVersion
 
 class ArchetypesServiceImpl @Inject() (archetypsDao: ArchetypeDao) extends ArchetypesService {
   
@@ -38,12 +39,44 @@ class ArchetypesServiceImpl @Inject() (archetypsDao: ArchetypeDao) extends Arche
   override def findAll: List[Archetype] = {
     archetypsDao.findAll
   }
-  
-  override def find(groupId: Option[String], artifactId: Option[String], version: Option[String]): List[Archetype] = {
-    archetypsDao.find(groupId, artifactId, version)
-  }
 
   def addAll(archetypes: List[Archetype]) = {
     archetypes.map { archetypsDao.safe }
   }
+  
+  override def find(groupId: Option[String], artifactId: Option[String], version: Option[String]): List[Archetype] = {
+    // This will be slow as hell, but I cannot slick, so...
+    val archetypes = archetypsDao.findAll.filter { a =>
+      if (groupId.isDefined) {
+        a.groupId.toLowerCase().contains(groupId.get.toLowerCase())
+      } else {
+        true
+      }
+    }.filter { a =>
+      if (artifactId.isDefined) {
+        a.artifactId.toLowerCase().contains(artifactId.get.toLowerCase())
+      } else {
+        true
+      }
+    }.filter { a =>
+      if (version.isDefined && version.get != "newest") {
+        a.version.toLowerCase().contains(version.get.toLowerCase())
+      } else {
+        true
+      }
+    }
+    if (version.isDefined && version.get == "newest") {
+      archetypes.groupBy( a => (a.groupId, a.artifactId)).flatMap {
+        case ((groupId, artifactId), list) => {
+          list.sortWith((a1, a2) => {
+            0 < a1.compareTo(a2)
+          }).take(1)
+        }
+      }.toList
+    } else {
+      archetypes
+    }
+  }
+  
+  implicit def archetypeToComparableVersion(a: Archetype) : ComparableVersion = new ComparableVersion(a.version)
 }
